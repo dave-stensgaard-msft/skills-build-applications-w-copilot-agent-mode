e5e39901c14fb558164e8aa672cacc262c4d9319
Dave Stensgaard <dave.stensgaard@microsoft.com>
2025-05-05 23:46:44 +0000

    Styled React components with Bootstrap for OctoFit Tracker

---
+++ octofit-tracker/frontend/src/components/Teams.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+function Teams() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    fetch('https://redesigned-happiness-xvjp5jr9r4hg54-8000.app.github.dev/api/teams/')
+      .then(response => response.json())
+      .then(data => setTeams(data))
+      .catch(error => console.error('Error fetching teams:', error));
+  }, []);
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h1 className="card-title">Teams</h1>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Team Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {teams.map(team => (
+              <tr key={team._id}>
+                <td>{team.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Teams;
